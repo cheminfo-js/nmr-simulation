2d906dc4d140bcd25f516b3776b819ca3540c983
Michaël Zasso <targos@protonmail.com>
2016-05-19 17:28:11 +0200

    spinus test

--- test.js
+++ test.js
@@ -1,48 +1,58 @@
 'use strict';
 
-/**
- * Created by acastillo on 5/19/16.
- */
+var newArray = require('new-array');
 var request = require('request');
 
-var molfile = "OCC(CC)CC\nActelion Java MolfileCreator 1.0\n\n 21 20  0  0  0  0  0  0  0  0999 V2000\n    6.5361   -0.9434    0.0000 O   0  0  0  0  0  0  0  0  0  0  0  0\n    4.9020    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    3.2680   -0.9434    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    3.2680   -2.8302    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    1.6340    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.0000   -0.9434    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    4.9020   -3.7735    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    8.3588   -0.4550   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n    5.5623    1.1439   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n    4.2415    1.1438   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n    3.2680    0.0000   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n    1.9472   -2.8304   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n    2.6077   -3.9741   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n    2.2944    1.1438   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n    0.9736    1.1438   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n   -1.1438   -1.6038   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.6604    0.2004   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n    0.6604   -2.0872   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n    6.0458   -4.4337   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n    4.2417   -4.9172   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n    5.5622   -2.6297   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n  2  1  1  0  0  0  0\n  3  2  1  1  0  0  0\n  4  3  1  0  0  0  0\n  5  3  1  0  0  0  0\n  6  5  1  0  0  0  0\n  7  4  1  0  0  0  0\n  1  8  1  0  0  0  0\n  2  9  1  1  0  0  0\n  2 10  1  0  0  0  0\n  3 11  1  0  0  0  0\n  4 12  1  1  0  0  0\n  4 13  1  0  0  0  0\n  5 14  1  1  0  0  0\n  5 15  1  0  0  0  0\n  6 16  1  0  0  0  0\n  6 17  1  0  0  0  0\n  6 18  1  0  0  0  0\n  7 19  1  0  0  0  0\n  7 20  1  0  0  0  0\n  7 21  1  0  0  0  0\nM  END\n";
+const nmr = require('.');
+
+var molfile = "CCc1cccc(C)c1\nJME 2016-03-06 Thu May 19 17:27:10 GMT+200 2016\n\n  9  9  0  0  0  0  0  0  0  0999 V2000\n    3.6373    0.7000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    3.6373    2.1000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    2.4249    2.8000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    1.2124    2.1000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    1.2124    0.7000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    2.4249    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    4.8497    2.8000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    6.0622    2.1000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.0000    2.8000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n  1  2  1  0  0  0  0\n  2  3  2  0  0  0  0\n  3  4  1  0  0  0  0\n  4  5  2  0  0  0  0\n  5  6  1  0  0  0  0\n  6  1  2  0  0  0  0\n  2  7  1  0  0  0  0\n  7  8  1  0  0  0  0\n  4  9  1  0  0  0  0\nM  END\n";
+
 request.post("http://www.nmrdb.org/service/predictor",{form:{molfile:molfile}},function(error, response, body){
-    console.log(body);
-    var lines = body.split("\n");
-    var nspins = lines.length;
-    var cs = new Array(nspins)
-    var integrals = new Array(nspins);
-    var ids = [];
-    var jc = new Array(nspins);
-    for(var i=0;i<nspins;i++){
-        jc[i] = new Array(nspins);
-        var values = lines[i].split("\t");
-        cs[i]=+values[2];
-        ids[i]=values[0]-1;
-        integrals[i]=+values[5];//Is it always 1??
-
-
-        var nCoup=(values.length-4)/3;
-        for(var j=0;j<nCoup;j++){
-            var withID = +values[4+3*i];
-            //if(spinusFormat==0)
-            withID=(withID-1)+"";
-            jc[i][+values[6+3*j]]=withID;
-        }
-
-        /*
-         for(int i=0;i<nCoup;i++){
-             String withID = tokens[4+spinusFormat+3*i];
-             //if(spinusFormat==0)
-             withID=(Integer.parseInt(withID)-1)+"";
-             peakPrediction.addJ(Double.parseDouble(tokens[6+spinusFormat+3*i]),withID);
-         }
-         */
-    }
+    var spinSystem = parseSpinusResult(body);
+    var simulation = nmr.simulate1D(spinSystem, {
+        frequency: 400.082470657773,
+        from: 0,
+        to: 11,
+        lineWidth: 1,
+        nbPoints: 16384
+    });
+    console.log(JSON.stringify(simulation));
 });
 
+function parseSpinusResult(body) {
+    var lines = body.split('\n');
+    var nspins = lines.length - 1;
+    var cs = new Array(nspins);
+    var integrals = new Array(nspins);
+    var ids = {};
+    var jc = new Array(nspins);
+    for (var i = 0; i < nspins; i++) {
+        jc[i] = newArray(nspins, 0);
+        var tokens = lines[i].split('\t');
+        cs[i] = +tokens[2];
+        ids[tokens[0] - 1] = i;
+        integrals[i] = +tokens[5];//Is it always 1??
 
-const nmr = require('.');
+
+        var nCoup = (tokens.length - 4) / 3;
+        for (var j = 0; j < nCoup; j++) {
+            var withID = tokens[4 + 3 * j] - 1;
+            var idx = ids[withID];
+            var value = +tokens[6 + 3 * j];
+            jc[i][idx] = value;
+        }
+    }
+    
+    for (var j = 0; j < nspins; j++) {
+        for (var i = j; i < nspins; i++) {
+            jc[j][i] = jc[i][j];
+        }
+    }
+
+    return new nmr.SpinSystem(cs, jc, newArray(nspins, 2));
+}
+
+/*
 
 const system = new nmr.SpinSystem(
     [1, 1, 0, 2, 2],
@@ -64,4 +74,6 @@ const simulation = nmr.simulate1D(system, {
     nbPoints: 16384
 });
 
-//console.log(JSON.stringify(simulation));
+console.log(JSON.stringify(simulation));
+
+*/
